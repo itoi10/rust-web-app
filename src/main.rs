3500da1f2adfb9f5a7074e89fbc70b2ab55dabb0
itict10 <itict10@gmail.com>
2023-09-11 23:07:17 +0900

    Update logger

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use web_prod::configuration::get_configuration;
 use web_prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // ログ出力を有効にする
-    // デフォルトのログレベルはinfoで、RUST_LOG環境変数で変更できる
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // すべてのログをsubscribersにリダイレクトする設定 (これでactix-webのログも出力される)
+    LogTracer::init().expect("Failed to set logger");
+    // 環境変数RUST_LOGに設定されたログレベルを読み込む
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    // Bunyan形式(JSON形式のログ)で標準出力に出力する
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        // ログレベルを設定
+        .with(env_filter)
+        // JSON形式でログ出力するための設定
+        .with(JsonStorageLayer)
+        // Bunyan形式でログを標準出力に出力する
+        .with(formatting_layer);
+    // アプリケーション全体でこのログ設定を使う
+    set_global_default(subscriber).expect("Failed to set subscriber");
 
     // 設定ファイルを読み込む
     let configuration = get_configuration().expect("Failed to read configuration.");
