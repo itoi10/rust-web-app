8af6199330cdddfe6fb41e7018c44d9f35eea1a4
itict10 <itict10@gmail.com>
2023-09-14 22:00:04 +0900

    Fix cargo clippy warning in PgPool::connect call

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     // Postgresに接続 (PgConnectionは単一のデータベース接続だが、PgPoolはコネクションプール)
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
+        PgPool::connect(configuration.database.connection_string().expose_secret())
             .await
             .expect("Failed to connect to Postgres.");
 
