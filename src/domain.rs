75399c50d51f5fea94acb138e49249c276c5f819
itict10 <itict10@gmail.com>
2023-09-21 22:03:56 +0900

    Add tests for SubscriberName validation

--- src/domain.rs
+++ src/domain.rs
@@ -6,6 +6,7 @@ pub struct NewSubscriber {
 }
 
 // サブスクライバーの名前を保持する構造体
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 impl SubscriberName {
@@ -33,5 +34,66 @@ impl SubscriberName {
 impl AsRef<str> for SubscriberName {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+// #[cfg(test)]をつけるとcargo testのときのみコンパイルされる
+// Rustでは特定のファイルやモジュール内にmod testsを定義してテストを書ける
+#[cfg(test)]
+mod tests {
+    // 別の名前空間となるので、SubscriberNameをインポートする
+    use crate::domain::SubscriberName;
+    use claim::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "ё".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    // 日本語の場合のテストも追加
+    #[test]
+    fn a_256_grapheme_long_japanese_name_is_valid() {
+        let name = "あ".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    // 日本語の場合のテストも追加
+    #[test]
+    fn a_japanese_name_longer_than_256_graphemes_is_rejected() {
+        let name = "あ".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::parse(name));
     }
 }
