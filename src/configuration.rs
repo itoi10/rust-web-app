a80f7c19c23f48b726077f9e46c71e56ce247969
itict10 <itict10@gmail.com>
2023-09-24 14:33:48 +0900

    Implement email client

--- src/configuration.rs
+++ src/configuration.rs
@@ -22,11 +22,19 @@ pub struct EmailClientSettings {
     pub base_url: String,
     // メールの送信者として設定するアドレス
     pub sender_email: String,
+    // Postmarkの認証トークン
+    pub authorization_token: Secret<String>,
+    // リクエストのタイムアウト時間
+    pub timeout_milliseconds: u64,
 }
 
 impl EmailClientSettings {
     pub fn sender(&self) -> Result<SubscriberEmail, String> {
         SubscriberEmail::parse(self.sender_email.clone())
+    }
+
+    pub fn timeout(&self) -> std::time::Duration {
+        std::time::Duration::from_millis(self.timeout_milliseconds)
     }
 }
 
